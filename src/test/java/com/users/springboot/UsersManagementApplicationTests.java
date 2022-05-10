a5b3d5575417bd07a48406f5e46de25214fdc59a
beneko <behzadnekouei@gmail.com>
2022-05-10 11:36:57 +0200

    create a spring boot starter project

---
+++ src/test/java/com/users/springboot/UsersManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
